473b0beb8d750702ef8b676b0c7dcb687a14f376
bossikram27-ui <bossikram27@gmail.com>
2026-08-13 00:00:12 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,16 @@
 
+BOT_TOKEN = "YOUR_NEW_BOT_TOKEN"
+
+ADMIN_ID = 8122367858
+
+SHIFT_START = "19:00"
+SHIFT_END = "07:00"
+
+FINE_RULES = {
+    "7:01-7:04": 100,
+    "7:05-7:09": 200,
+    "7:10+": 500
+}
+
+DATABASE = "attendance.db"
+EXCEL_FILE = "attendance.xlsx"
